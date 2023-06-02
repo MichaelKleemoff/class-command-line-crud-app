c6be39c4dc1260a0a0e74d93aa7d17c0e882d192
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-06-02 13:14:22 -0400

    Start animalsController.js and associated index.js code.

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const { readJSONFile, writeJSONFile } = require('./src/helpers');
+const {
+	create,
+	destroy,
+	edit,
+	index,
+	show,
+} = require('./src/animalsController');
 
-let animals = readJSONFile('./data', 'animals.json');
+// Intention: Alias for `console.log`. `console.log` is for the developer. `inform` is for the user of our app.
+const inform = console.log;
 
 function run() {
-	console.log('Peace');
+	const action = process.argv[2]; // What `action` did the user type in?
+	const animal = process.argv[3];
+	let animals = readJSONFile('./data', 'animals.json');
+
+	console.log('List of arguments for this `process.argv`', process.argv);
+	console.log('Here is our `animals` data: ', animals);
+
+	inform('Welcome to our Animals App 🙉🙈🐵🐒\n\n');
+
+	switch (action) {
+		case 'index':
+			const animalsView = index(animals);
+			inform(animalsView);
+			break;
+		case 'show':
+			const animalView = show(animals, animal);
+			inform(animalView);
+			break;
+		default:
+			inform('There was an error.');
+	}
 }
 
 run();
---
+++ src/animalsController.js
@@ -0,0 +1,16 @@
+function index(animals) {
+	return animals.map((animal) => animal.id + ' ' + animal.name).join('\n');
+}
+
+function show(animals, animalId) {
+	const animal = animals.find((animal) => animal.id === animalId);
+	return animal.id + ' ' + animal.name + ' ' + animal.points + ' ' + ' points';
+}
+
+module.exports = {
+	create,
+	destroy,
+	edit,
+	index,
+	show,
+};
